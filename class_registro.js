0efc9bdde96a768ae1726b2fdaef7d6b3cf1af71
LaloSoftware <eduardo.lemus.laguna@gmail.com>
2019-02-12 21:14:10 -0600

    creación del esqueleto de las clases en js

---
+++ class_registro.js
@@ -0,0 +1,17 @@
+class Registro {
+  constructor() {
+
+  }
+
+  public var articulos;
+  public var cliente = "Venta al publico";
+  public var fecha_y_hora;
+
+  public RegistrarIngresoMercancia(articulos){
+
+  }
+
+  public registrarVenta(){
+
+  }
+}
